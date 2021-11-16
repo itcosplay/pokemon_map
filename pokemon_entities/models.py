0ea5ca2c1b04fc7bb947b5fba7708e0c7dcc1f07
itcosplay <azure_dream@mail.ru>
2021-11-16 13:50:13 +0300

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True)
@@ -11,5 +12,6 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=CASCADE)
     lat = models.FloatField(null=True)
     lon = models.FloatField(null=True)
